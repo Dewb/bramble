05bae5efe448e4ed83549c5335dead01462e4d21
Michael Dewberry <michael.dewberry@gmail>
2015-08-23 22:02:53 -0400

    Add more variation; wait for button to go low before resetting

--- bramble_speech.ino
+++ bramble_speech.ino
@@ -8,6 +8,8 @@
 #include <Bounce2.h>
 #include <EEPROM.h>
 
+#define DEBUG_OUTPUT false
+#define dbgOut(X) { if (DEBUG_OUTPUT) { Serial.println(X); }}
 #define EEPROM_SEED_LOCATION 0
 
 #define MODE_SWITCH_PIN 3
@@ -56,7 +58,8 @@ int heaviest_intensity = 100;
 int heaviest_duration = 140;
 int caesura_duration = 270;
 
-int debounce_time = 40;
+int random_duration_range = 10;
+int random_intensity_range = 15;
 int solenoid_open_time = 120;
 int thinking_time_min = 1500;
 int thinking_time_max = 5500;
@@ -155,7 +158,11 @@ void create_rhythm_common_metre(char* phrase, int lines) {
     if (line % 2 == 0) {
       phrase[index++] = SYL_LIGHT;
       phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
-      phrase[index++] = SYL_LIGHT;
+      if (random(0, 5) == 0) {
+        phrase[index++] = SYL_DBLLIGHT;
+      } else {
+        phrase[index++] = SYL_LIGHT;
+      }
       phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
       if (random(0, 4) == 0) {
         phrase[index++] = SYL_BEAT;
@@ -163,18 +170,22 @@ void create_rhythm_common_metre(char* phrase, int lines) {
       phrase[index++] = SYL_LIGHT;
       phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
       phrase[index++] = SYL_LIGHT;
-      phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
+      phrase[index++] = pickFromThree(SYL_HEAVY, SYL_HEAVIER, SYL_HEAVIEST);
       phrase[index++] = SYL_BEAT;
     } else {
       phrase[index++] = SYL_LIGHT;
-      phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
-      phrase[index++] = SYL_LIGHT;
+      phrase[index++] = pickFromThree(SYL_HEAVY, SYL_HEAVIER, SYL_HEAVIEST);
+      if (random(0, 5) == 0) {
+        phrase[index++] = SYL_DBLLIGHT;
+      } else {
+        phrase[index++] = SYL_LIGHT;
+      }
       phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
       phrase[index++] = SYL_LIGHT;
       if (random(0, 4) == 0) {
         phrase[index++] = SYL_BEAT;
       }
-      phrase[index++] = pickFromTwo(SYL_HEAVY, SYL_HEAVIER);
+      phrase[index++] = pickFromThree(SYL_HEAVY, SYL_HEAVIER, SYL_HEAVIEST);
       phrase[index++] = SYL_CAESURA;
     }
   }
@@ -189,8 +200,8 @@ void toggleSolenoid(int ms_on, int ms_off) {
 }
 
 void speakSolenoidSyllable(int syllableNominalLength, int intensity, int duration) {
-  int ms_on = solenoid_open_time * (intensity / 100.0);
-  int ms_total = (duration / 100.0) * syllableNominalLength;
+  int ms_on = solenoid_open_time * ((intensity + random(-random_intensity_range, random_intensity_range)) / 100.0);
+  int ms_total = syllableNominalLength * ((duration + random(-random_duration_range, random_duration_range)) / 100.0);
   int ms_off = ms_total > ms_on ? ms_total - ms_on : 20;
   toggleSolenoid(ms_on, ms_off);
 }
@@ -198,6 +209,7 @@ void speakSolenoidSyllable(int syllableNominalLength, int intensity, int duratio
 void talk() {
   digitalWrite(BUTTON_LED_PIN, LOW);
   
+  dbgOut("Thinking...");
   delay(thinking_time_min + random(0, thinking_time_max - thinking_time_min));
   
   // guarantee long stochastic arc across reboots (h/t Giles Hall)
@@ -209,10 +221,13 @@ void talk() {
   
   int meterChoice = random(0, 3);
   if (meterChoice == 0) {
+    dbgOut("Divining a Saturnian quip...");
     create_rhythm_saturnian(phrase, 2);
   } else if (meterChoice == 1) {
+    dbgOut("Penning a verse of iambic pentameter...");
     create_rhythm_iambic_pentameter(phrase, 4);
   } else {
+    dbgOut("Composing a common-metre hymn...");
     create_rhythm_common_metre(phrase, 4);
   }
   
@@ -220,6 +235,8 @@ void talk() {
   
   int i = 0;
   float dc = 1.0;
+  
+  dbgOut("Talking...");
   
   while (phrase[i] != PHRASE_END && i < PHRASE_MAX_LENGTH) {
     switch (phrase[i]) {
@@ -265,7 +282,10 @@ void talk() {
     dc += intensity_drain_correction;
   }
   
+  dbgOut("Pausing to refill accumulators...");
   delay(refill_time);
+  dbgOut("Done talking.");
+  
   digitalWrite(BUTTON_LED_PIN, HIGH);
 }
 
@@ -283,9 +303,14 @@ void setup() {
   
   digitalWrite(SOLENOID_PIN, LOW);
   digitalWrite(BUTTON_LED_PIN, HIGH);
+  
+  if (DEBUG_OUTPUT) {
+    Serial.begin(9600);
+  }
 }
 
 void loop() {
+  
   modeSwitchBouncer.update();
   buttonBouncer.update();
   
@@ -305,6 +330,12 @@ void loop() {
       digitalWrite(SOLENOID_PIN, LOW);
     }  
   } else if (mode == MODE_SPEECH && buttonState == LOW) {
-    talk();    
+    talk();  
+    while (buttonState == LOW) {
+      buttonBouncer.update();
+      buttonState = buttonBouncer.read();
+    }
+    dbgOut("Ready.");    
   }
+  
 }
